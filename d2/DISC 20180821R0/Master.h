20fca279b72daefb3b75a586b6aac629b3f747f5
jeonstin <jjw5245@gmail.com>
2019-01-21 17:15:28 +0900

    한양 디스크라인 D2 최종본

---
+++ d2/DISC 20180821R0/Master.h
@@ -0,0 +1,110 @@
+/**************************************************************************/
+/* LabWindows/CVI User Interface Resource (UIR) Include File              */
+/* Copyright (c) National Instruments 2018. All Rights Reserved.          */
+/*                                                                        */
+/* WARNING: Do not add to, delete from, or otherwise modify the contents  */
+/*          of this include file.                                         */
+/**************************************************************************/
+
+#include <userint.h>
+
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
+     /* Panels and Controls: */
+
+#define  ZEROSETUP                        1       /* callback function: callZerosetup */
+#define  ZEROSETUP_DATA_JNL               2       /* control type: table, callback function: ZeroDataSetup */
+#define  ZEROSETUP_TEXTMSG_2              3       /* control type: textMsg, callback function: (none) */
+#define  ZEROSETUP_DOZEROSET_JNL_2        4       /* control type: command, callback function: DoZeroSet2 */
+#define  ZEROSETUP_DOZEROSET_JNL          5       /* control type: command, callback function: DoZeroSet */
+#define  ZEROSETUP_DOZEROSET_AIR_P        6       /* control type: command, callback function: DoZeroSet2 */
+#define  ZEROSETUP_TEXTBOX                7       /* control type: textBox, callback function: (none) */
+#define  ZEROSETUP_DOZEROSET_AIR_N        8       /* control type: command, callback function: DoZeroSet1 */
+#define  ZEROSETUP_TIP_COMMAND            9       /* control type: command, callback function: Tip_Command */
+#define  ZEROSETUP_QUIT_TEXTMSG           10      /* control type: textMsg, callback function: (none) */
+#define  ZEROSETUP_DECORATION_3           11      /* control type: deco, callback function: (none) */
+#define  ZEROSETUP_AIR_ZERO_2             12      /* control type: command, callback function: AirZeroSet2 */
+#define  ZEROSETUP_AIR_ZERO_1             13      /* control type: command, callback function: AirZeroSet1 */
+#define  ZEROSETUP_COMMANDBUTTON_7        14      /* control type: command, callback function: (none) */
+#define  ZEROSETUP_COMMANDBUTTON_4        15      /* control type: command, callback function: (none) */
+#define  ZEROSETUP_COMMANDBUTTON_9        16      /* control type: command, callback function: (none) */
+#define  ZEROSETUP_COMMANDBUTTON_6        17      /* control type: command, callback function: (none) */
+#define  ZEROSETUP_COMMANDBUTTON_8        18      /* control type: command, callback function: (none) */
+#define  ZEROSETUP_T_NAME_3               19      /* control type: command, callback function: (none) */
+#define  ZEROSETUP_T_NAME_4               20      /* control type: command, callback function: (none) */
+#define  ZEROSETUP_COMMANDBUTTON_10       21      /* control type: command, callback function: (none) */
+#define  ZEROSETUP_CH_NAME2               22      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_COMMANDBUTTON_5        23      /* control type: command, callback function: (none) */
+#define  ZEROSETUP_T_NAME1_2              24      /* control type: textMsg, callback function: (none) */
+#define  ZEROSETUP_COMMANDBUTTON_2        25      /* control type: command, callback function: (none) */
+#define  ZEROSETUP_T_NAME_2               26      /* control type: command, callback function: (none) */
+#define  ZEROSETUP_DIS_R_2                27      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_T_NAME                 28      /* control type: command, callback function: (none) */
+#define  ZEROSETUP_NLIMIT_2               29      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_CH_NAME1               30      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_CH_NAME                31      /* control type: textMsg, callback function: (none) */
+#define  ZEROSETUP_MASTER_HV2             32      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_T_NAME1_1              33      /* control type: textMsg, callback function: (none) */
+#define  ZEROSETUP_MASTER_LV2             34      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_TEXTMSG                35      /* control type: textMsg, callback function: (none) */
+#define  ZEROSETUP_MASTER_HV              36      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_DIS_R_1                37      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_MASTER_LV              38      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_M_HIGH_3               39      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_M_HIGH_2               40      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_M_HIGH_1               41      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_NLIMIT_1               42      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_M_LOW_3                43      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_SPLITTER               44      /* control type: splitter, callback function: (none) */
+#define  ZEROSETUP_M_LOW_2                45      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_A_HIGH_3               46      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_M_LOW_1                47      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_SPLITTER_3             48      /* control type: splitter, callback function: (none) */
+#define  ZEROSETUP_A_LOW_3                49      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_A_HIGH_2               50      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_A_HIGH_1               51      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_SPLITTER_2             52      /* control type: splitter, callback function: (none) */
+#define  ZEROSETUP_A_LOW_2                53      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_A_LOW_1                54      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_DECORATION             55      /* control type: deco, callback function: (none) */
+#define  ZEROSETUP_KEYBOARD               56      /* control type: command, callback function: Keyboard */
+#define  ZEROSETUP_DOZEROSET_JNL_4        57      /* control type: command, callback function: DoZeroSet4 */
+#define  ZEROSETUP_DOZEROSET_JNL_3        58      /* control type: command, callback function: DoZeroSet3 */
+#define  ZEROSETUP_LED_3                  59      /* control type: LED, callback function: (none) */
+#define  ZEROSETUP_ENC                    60      /* control type: numeric, callback function: (none) */
+#define  ZEROSETUP_CH_TIMER               61      /* control type: timer, callback function: Ch_Timer */
+#define  ZEROSETUP_CLOSE                  62      /* control type: command, callback function: Close */
+
+
+     /* Control Arrays: */
+
+          /* (no control arrays in the resource file) */
+
+
+     /* Menu Bars, Menus, and Menu Items: */
+
+          /* (no menu bars in the resource file) */
+
+
+     /* Callback Prototypes: */
+
+int  CVICALLBACK AirZeroSet1(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK AirZeroSet2(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK callZerosetup(int panel, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK Ch_Timer(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK Close(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK DoZeroSet(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK DoZeroSet1(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK DoZeroSet2(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK DoZeroSet3(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK DoZeroSet4(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK Keyboard(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK Tip_Command(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK ZeroDataSetup(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+
+
+#ifdef __cplusplus
+    }
+#endif
